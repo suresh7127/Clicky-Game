3940302455cccbc002a8100b19c7d3849be091c9
suresh <sureshmahato@Sureshs-MacBook-Pro.local>
2019-09-04 17:44:27 -0500

    card suffle function coded for app.js file

--- src/App.js
+++ src/App.js
@@ -2,28 +2,39 @@ import react from 'react';
 import header from './components/header';
 import wrapper from './components/wrapper';
 import card from './components/card';
-import clicked from "./clicked.json";
+import character from "./character.json";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
 
-export default App;
+  state = {
+    character,
+    score: 0,
+    highscore: 10,
+    message: "Click on Images to begin the game!"
+  }
+
+  cardsuffle = (id) => {
+    if (this.state.character[id - 1].clicked) {
+      this.setState({
+        message: "wrong guess, Try again!",
+
+      });
+      // count score
+    } else {
+      this.setState({
+        score: this.state.score += 1,
+        message: "Right guess!",
+
+        character: this.state.character.sort(() => Math.random() - 0.5)
+      });
+    }
+    // count highscore
+    if (this.state.highscore < this.state.score) {
+      this.setState({
+        highscore: this.state.score
+      })
+    }
+  }
+
+  export default App;
